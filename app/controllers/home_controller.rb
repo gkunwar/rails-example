deb1d027c423bf8037f60c07bcab262dc0f21ab6
gkunwar <gkunwar09@gmail.com>
2017-04-01 20:42:48 +0545

    Clean existing site

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,5 @@
 class HomeController < ApplicationController
-  # layout 'contact'
   def index
-    @name = "Ganesh"
-    # It gives the number of record from events table
-    @event_count = Event.count
-    # It gives first record from events table
-    @event = Event.first
-    address = 'address'
   end
 
   def contact
@@ -14,13 +7,8 @@ class HomeController < ApplicationController
   end
 
   def event_contact
-    # Redirect to contact
-    # redirect_to '/contact'
-    # Render
-    # render 'contact'
   end
 
   def search
-    binding.pry
   end
 end
